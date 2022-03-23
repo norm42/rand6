618f9b81c269a82eb965bb7cfc672a17bd791b98
Norm Zeck <norm86@gmail.com>
2022-03-23 11:14:23 -0400

    Added comments

--- main.py
+++ main.py
@@ -1,21 +1,26 @@
-# This is a sample Python script.
+# we use the python random lib to generate the dice
 import random as ra
 
 
 
-# Press the green button in the gutter to run the script.
+# Test program for dice generation.
+# This one generates a random sequence of dice with unique values
 if __name__ == '__main__':
     dice = []
     got_all = False
     tally = 0
     while not got_all:
+        # randrange is [), so need to use [1,7)
+        # by integer values to get dice 1-6
         dice_num = ra.randrange(1, 7, 1)
+        # check to see if the dice number does not exist
+        # if so then add
         if dice_num not in dice:
             dice.append(dice_num)
             tally = tally + 1
         if tally == 6:
-            got_all = True
-        print(dice)
+            got_all = True  # we have all 6 slots filled
+        print(dice)     # visualize the iterations
 
 
 
